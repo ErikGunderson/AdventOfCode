9e36b5fb9c071092b705c6824eeaf5ef1223dbb5
Erik <gundy.goo@gmail.com>
2020-12-08 08:01:06 -0600

    Completed day 8

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,7 @@ import solutions.*
 
 fun main() {
     try {
-        Day8().solution1()
+        Day8().solution2()
     } catch (exception: Exception) {
         print("\n ${exception.stackTrace.contentToString()} \n")
     }
--- src/main/kotlin/solutions/Day8.kt
+++ src/main/kotlin/solutions/Day8.kt
@@ -4,10 +4,107 @@ import FileReader
 
 class Day8 : FileReader() {
     fun solution1() {
+        var accumulator = 0
+        val instructions = mutableListOf<Instruction>()
+        inputFile.readLines().forEachIndexed { index, input ->
+            instructions.add(input.split(" ").let { Instruction(it[0], it[1].toInt(), index) })
+        }
 
+        var shouldExecuteLoop = true
+        var currentIndex = 0
+
+        while(shouldExecuteLoop) {
+            instructions[currentIndex].let {
+                if (it.executionCount >= 1) {
+                    shouldExecuteLoop = false
+                    return@let
+                }
+
+                when (it.instruction) {
+                    "acc" -> {
+                        accumulator += it.amount
+                        currentIndex += 1
+                    }
+                    "jmp" -> {
+                        currentIndex += it.amount
+                    }
+                    "nop" -> {
+                        currentIndex += 1
+                    }
+                }
+
+                it.executionCount += 1
+            }
+        }
+
+        print("Accumulator value is: $accumulator")
+        print("\nDONE :D")
     }
 
     fun solution2() {
+        var accumulator = 0
 
+        val instructions = mutableListOf<Instruction>()
+        inputFile.readLines().forEachIndexed { index, input ->
+            instructions.add(input.split(" ").let { Instruction(it[0], it[1].toInt(), index) })
+        }
+
+        var shouldExecuteLoop = true
+        var currentIndex = 0
+
+        instructions.mapNotNull { if (it.instruction == "nop" || it.instruction == "jmp") it else null }.forEach {
+            val originalInstruction = if (it.instruction == "nop") {
+                it.instruction = "jmp"
+                "nop"
+            } else {
+                it.instruction = "nop"
+                "jmp"
+            }
+
+            while(shouldExecuteLoop) {
+                if (currentIndex == instructions.size) {
+                    shouldExecuteLoop = false
+                    print("Accumulator value is: $accumulator")
+                    print("\nDONE :D")
+                    return
+                }
+
+                instructions[currentIndex].let {
+                    if (it.executionCount >= 1) {
+                        shouldExecuteLoop = false
+//                        print ("executed ${it.instruction} at line $currentIndex twice")
+                        return@let
+                    }
+
+                    when (it.instruction) {
+                        "acc" -> {
+                            accumulator += it.amount
+                            currentIndex += 1
+                        }
+                        "jmp" -> {
+                            currentIndex += it.amount
+                        }
+                        "nop" -> {
+                            currentIndex += 1
+                        }
+                    }
+
+                    it.executionCount += 1
+                }
+            }
+
+            accumulator = 0
+            shouldExecuteLoop = true
+            currentIndex = 0
+            instructions.forEach { it.executionCount = 0 }
+            it.instruction = originalInstruction
+        }
     }
 }
+
+data class Instruction(
+    var instruction: String,
+    val amount: Int,
+    var instructionIndex: Int,
+    var executionCount: Int = 0
+)
