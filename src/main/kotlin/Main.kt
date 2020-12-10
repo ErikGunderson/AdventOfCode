977288f3cc376ed519c384aa292dfc1f016f0984
Erik <gundy.goo@gmail.com>
2020-12-10 10:05:23 -0600

    Completed day 10.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,7 @@ import solutions.*
 
 fun main() {
     try {
-        Day9().solution2()
+        Day10().solution2()
     } catch (exception: Exception) {
         print("\n ${exception.stackTrace.contentToString()} \n")
     }
---
+++ src/main/kotlin/solutions/Day10.kt
@@ -0,0 +1,65 @@
+package solutions
+
+import FileReader
+
+class Day10 : FileReader() {
+    fun solution1() {
+        var oneJoltDiffs = 0
+        var threeJoltDiffs = 1
+
+        inputFile.readLines().map { it.toInt() }.sorted().let {
+            it.zipWithNext { current, next ->
+                when (next - current) {
+                    1 -> oneJoltDiffs += 1
+                    3 -> threeJoltDiffs += 1
+                }
+            }
+
+            when (it[0] - 0) {
+                1 -> oneJoltDiffs += 1
+                3 -> threeJoltDiffs += 1
+            }
+        }
+
+        print("1 Jolt diffs: $oneJoltDiffs; 3 Jolt diffs: $threeJoltDiffs; multiplied is: ${oneJoltDiffs * threeJoltDiffs}")
+        print("\nDONE :D")
+    }
+
+    fun solution2() {
+        inputFile.readLines()
+            .map { Adapter(it.toInt()) }
+            .toMutableList()
+            .apply { add(Adapter(0)) }
+            .apply { add(Adapter(maxOf { it.jolts } + 3, possibleExitConnections = 1)) }
+            .sortedByDescending { it.jolts }
+            .let { inputSet ->
+                inputSet.forEachIndexed { index, adapter ->
+                    var continueLoop = true
+                    var currentIndexOffset = 1
+
+                    while (continueLoop) {
+                        val currentIndex = index - currentIndexOffset
+
+                        if (currentIndex < 0) {
+                            continueLoop = false
+                        } else {
+                            val currentJoltDiff = inputSet[currentIndex].jolts - adapter.jolts
+
+                            if (currentJoltDiff in 1..3) {
+                                adapter.possibleExitConnections += inputSet[currentIndex].possibleExitConnections
+                                currentIndexOffset += 1
+                            } else continueLoop = false
+                        }
+                    }
+                }
+
+                print("\ntotal adapter combinations: ${inputSet[inputSet.lastIndex].possibleExitConnections}")
+                print("\nDONE :D")
+            }
+    }
+}
+
+data class Adapter(
+    val jolts: Int,
+    var possibleExitConnections: Long = 0
+)
