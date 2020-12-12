793405d5727da119b2016cdb3175096202f9256b
Erik <gundy.goo@gmail.com>
2020-12-12 12:00:08 -0600

    Completed day 12.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,7 @@ import solutions.*
 
 fun main() {
     try {
-        Day11().solution2()
+        Day12().solution2()
     } catch (exception: Exception) {
         print("\n ${exception.stackTrace.contentToString()} \n")
     }
--- src/main/kotlin/solutions/Day12.kt
+++ src/main/kotlin/solutions/Day12.kt
@@ -1,13 +1,136 @@
 package solutions
 
 import FileReader
+import solutions.CardinalDirection.*
+import kotlin.math.abs
 
 class Day12 : FileReader() {
     fun solution1() {
+        val cardinalMovementMap = mutableMapOf(
+            North to 0,
+            South to 0,
+            East to 0,
+            West to 0
+        )
+        var currentFacing = East
 
+        inputFile.readLines().forEach {
+            val direction = it.first()
+            var units = it.substring(1..it.lastIndex).toInt()
+
+            when (direction) {
+                'N' -> cardinalMovementMap[North] = cardinalMovementMap[North]!!.plus(units)
+                'S' -> cardinalMovementMap[South] = cardinalMovementMap[South]!!.plus(units)
+                'E' -> cardinalMovementMap[East] = cardinalMovementMap[East]!!.plus(units)
+                'W' -> cardinalMovementMap[West] = cardinalMovementMap[West]!!.plus(units)
+                'L' -> {
+                    while (units > 0) {
+                        units -= 90
+                        currentFacing = currentFacing.turnLeft()
+                    }
+                }
+                'R' -> {
+                    while (units > 0) {
+                        units -= 90
+                        currentFacing = currentFacing.turnRight()
+                    }
+                }
+                'F' -> cardinalMovementMap[currentFacing] = cardinalMovementMap[currentFacing]!!.plus(units)
+            }
+        }
+
+        val manhattanDistance = abs(cardinalMovementMap[North]!!.minus(cardinalMovementMap[South]!!)) +
+                abs(cardinalMovementMap[East]!!.minus(cardinalMovementMap[West]!!))
+
+        print("Ship's manhattan distance is: $manhattanDistance")
+        print("\nDONE :D")
     }
 
     fun solution2() {
+        val shipCardinalMovementMap = mutableMapOf(
+            North to 0,
+            South to 0,
+            East to 0,
+            West to 0
+        )
+        val waypointOffsetMap = mutableMapOf(
+            North to 1,
+            South to 0,
+            East to 10,
+            West to 0
+        )
 
+        inputFile.readLines().forEach {
+            val direction = it.first()
+            var units = it.substring(1..it.lastIndex).toInt()
+
+            when (direction) {
+                'N' -> waypointOffsetMap[North] = waypointOffsetMap[North]!!.plus(units)
+                'S' -> waypointOffsetMap[South] = waypointOffsetMap[South]!!.plus(units)
+                'E' -> waypointOffsetMap[East] = waypointOffsetMap[East]!!.plus(units)
+                'W' -> waypointOffsetMap[West] = waypointOffsetMap[West]!!.plus(units)
+                'L' -> {
+                    while (units > 0) {
+                        val newWaypointNorth = waypointOffsetMap[East]!!
+                        val newWaypointEast = waypointOffsetMap[South]!!
+                        val newWaypointSouth = waypointOffsetMap[West]!!
+                        val newWaypointWest = waypointOffsetMap[North]!!
+
+                        waypointOffsetMap[North] = newWaypointNorth
+                        waypointOffsetMap[East] = newWaypointEast
+                        waypointOffsetMap[South] = newWaypointSouth
+                        waypointOffsetMap[West] = newWaypointWest
+
+                        units -= 90
+                    }
+                }
+                'R' -> {
+                    while (units > 0) {
+                        val newWaypointNorth = waypointOffsetMap[West]!!
+                        val newWaypointEast = waypointOffsetMap[North]!!
+                        val newWaypointSouth = waypointOffsetMap[East]!!
+                        val newWaypointWest = waypointOffsetMap[South]!!
+
+                        waypointOffsetMap[North] = newWaypointNorth
+                        waypointOffsetMap[East] = newWaypointEast
+                        waypointOffsetMap[South] = newWaypointSouth
+                        waypointOffsetMap[West] = newWaypointWest
+
+                        units -= 90
+                    }
+                }
+                'F' -> {
+                    for (i in 0 until units) {
+                        shipCardinalMovementMap[North] = shipCardinalMovementMap[North]!!.plus(waypointOffsetMap[North]!!)
+                        shipCardinalMovementMap[South] = shipCardinalMovementMap[South]!!.plus(waypointOffsetMap[South]!!)
+                        shipCardinalMovementMap[East] = shipCardinalMovementMap[East]!!.plus(waypointOffsetMap[East]!!)
+                        shipCardinalMovementMap[West] = shipCardinalMovementMap[West]!!.plus(waypointOffsetMap[West]!!)
+                    }
+                }
+            }
+        }
+
+        val manhattanDistance = abs(shipCardinalMovementMap[North]!!.minus(shipCardinalMovementMap[South]!!)) +
+                abs(shipCardinalMovementMap[East]!!.minus(shipCardinalMovementMap[West]!!))
+
+        print("Ship's manhattan distance is: $manhattanDistance")
+        print("\nDONE :D")
     }
+}
+
+enum class CardinalDirection {
+    North,
+    East,
+    South,
+    West
+}
+
+fun CardinalDirection.turnLeft() : CardinalDirection {
+    val newOrdinal = (this.ordinal - 1).let { if (it < 0) CardinalDirection.values().lastIndex else it }
+    return CardinalDirection.values()[newOrdinal]
+}
+
+fun CardinalDirection.turnRight() : CardinalDirection {
+    val newOrdinal = (this.ordinal + 1).let { if (it > CardinalDirection.values().lastIndex) 0 else it }
+    return CardinalDirection.values()[newOrdinal]
 }
