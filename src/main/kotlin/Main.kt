05568ffb07ba15c3019f16ec1c151f698463d7f9
Erik <gundy.goo@gmail.com>
2021-12-02 08:38:25 -0600

    Finished day 2.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,9 @@
 import solutions.aoc2021.Day1
+import solutions.aoc2021.Day2
 
 fun main() {
     try {
-        Day1().solution2()
+        Day2().solution2()
     } catch (exception: Exception) {
         print("\n$exception ${exception.stackTrace.map { "\nat -> $it" }.joinToString("")} \n")
     }
---
+++ src/main/kotlin/solutions/aoc2021/Day2.kt
@@ -0,0 +1,47 @@
+package solutions.aoc2021
+
+import AoC2021Problem
+
+class Day2: AoC2021Problem() {
+    fun solution1() {
+        val coords = SubCoordinate()
+        inputFile.readLines().map { it.trim() }.forEach { input ->
+            Regex("\\s+(?=\\d)").splitToSequence(input, 2).iterator().let {
+                when (it.next().trim()) {
+                    "forward" -> coords.horizontalPosition += it.next().toInt()
+                    "down" -> coords.depth += it.next().toInt()
+                    "up" -> coords.depth -= it.next().toInt()
+                }
+            }
+        }
+
+        print("Depth is ${coords.depth}, horizontal position is ${coords.horizontalPosition}, product is ${coords.depth * coords.horizontalPosition}")
+        print("\nDONE :D")
+    }
+
+    fun solution2() {
+        val coords = SubCoordinate()
+        inputFile.readLines().map { it.trim() }.forEach { input ->
+            Regex("\\s+(?=\\d)").splitToSequence(input, 2).iterator().let {
+                when (it.next().trim()) {
+                    "forward" -> {
+                        val movementAmount = it.next().toInt()
+                        coords.horizontalPosition += movementAmount
+                        coords.depth += coords.aim * movementAmount
+                    }
+                    "down" -> coords.aim += it.next().toInt()
+                    "up" -> coords.aim -= it.next().toInt()
+                }
+            }
+        }
+
+        print("Depth is ${coords.depth}, horizontal position is ${coords.horizontalPosition}, product is ${coords.depth * coords.horizontalPosition}")
+        print("\nDONE :D")
+    }
+}
+
+class SubCoordinate(
+    var depth: Int = 0,
+    var horizontalPosition: Int = 0,
+    var aim: Int = 0
+)
