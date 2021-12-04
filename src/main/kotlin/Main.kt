9e56123cb9ef3f506a695377ccff59f8fd8d2261
Erik <gundy.goo@gmail.com>
2021-12-04 01:17:44 -0600

    completed day 4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,8 @@
-import solutions.aoc2021.Day1
-import solutions.aoc2021.Day2
-import solutions.aoc2021.Day3
+import solutions.aoc2021.Day4
 
 fun main() {
     try {
-        Day3().solution2()
+        Day4().solution2()
     } catch (exception: Exception) {
         print("\n$exception ${exception.stackTrace.map { "\nat -> $it" }.joinToString("")} \n")
     }
---
+++ src/main/kotlin/solutions/aoc2021/Day4.kt
@@ -0,0 +1,137 @@
+package solutions.aoc2021
+
+import AoC2021Problem
+
+class Day4 : AoC2021Problem() {
+    fun solution1() {
+        val numbersDrawn = mutableListOf<Int>()
+        var boardNumber = 0
+        val boardNumbers = Array(100) { index -> BoardNumber(index) }
+        val boards = mutableMapOf<Int, MutableList<List<BoardNumber>>>()
+
+        //Assemble boards
+        inputFile.readLines().map { it.trim() }.let { inputLines ->
+            inputLines.forEachIndexed { index, inputLine ->
+                when {
+                    index == 0 -> numbersDrawn.addAll(inputLine.split(",").map { it.trim().toInt() })
+                    inputLine.isBlank() -> {
+                        if (boardNumber != 0) {
+                            val rows = boards[boardNumber]!!
+                            val columns = mutableListOf<List<BoardNumber>>()
+                            for (rowNumber in 0..rows.lastIndex) {
+                                columns.add(rows.map { it[rowNumber] })
+                            }
+                            boards[boardNumber]!!.addAll(columns)
+                        }
+
+                        boardNumber++
+                        boards[boardNumber] = mutableListOf()
+                    }
+                    else -> {
+                        val numbersInRow = inputLine.split(Regex("\\s+(?=\\d)")).map { it.toInt() }
+                        boards[boardNumber]!!.add(numbersInRow.map { boardNumbers[it] })
+
+                        if (index == inputLines.lastIndex) {
+                            val rows = boards[boardNumber]!!
+                            val columns = mutableListOf<List<BoardNumber>>()
+                            for (rowNumber in 0..rows.lastIndex) {
+                                columns.add(rows.map { it[rowNumber] })
+                            }
+                            boards[boardNumber]!!.addAll(columns)
+                        }
+                    }
+                }
+            }
+        }
+
+        //Play game until a board wins
+        numbersDrawn.forEach playGame@{ currentNumber ->
+            boardNumbers[currentNumber].marked = true
+
+            boards.values.forEach { board ->
+                board.forEach { rowOrColumn ->
+                    if (rowOrColumn.all { it.marked }) {
+                        //calculate score
+                        val boardScore = board.flatten().distinctBy { it.value }.filterNot { it.marked }.map { it.value }.sum()
+
+                        print("Board score is $boardScore, final number called is $currentNumber, product is ${boardScore * currentNumber}")
+                        print("\nDONE :D")
+                        return
+                    }
+                }
+            }
+        }
+    }
+
+    fun solution2() {
+        val numbersDrawn = mutableListOf<Int>()
+        var boardNumber = 0
+        val boardNumbers = Array(100) { index -> BoardNumber(index) }
+        val boards = mutableMapOf<Int, MutableList<List<BoardNumber>>>()
+
+        //Assemble boards
+        inputFile.readLines().map { it.trim() }.let { inputLines ->
+            inputLines.forEachIndexed { index, inputLine ->
+                when {
+                    index == 0 -> numbersDrawn.addAll(inputLine.split(",").map { it.trim().toInt() })
+                    inputLine.isBlank() -> {
+                        if (boardNumber != 0) {
+                            val rows = boards[boardNumber]!!
+                            val columns = mutableListOf<List<BoardNumber>>()
+                            for (rowNumber in 0..rows.lastIndex) {
+                                columns.add(rows.map { it[rowNumber] })
+                            }
+                            boards[boardNumber]!!.addAll(columns)
+                        }
+
+                        boardNumber++
+                        boards[boardNumber] = mutableListOf()
+                    }
+                    else -> {
+                        val numbersInRow = inputLine.split(Regex("\\s+(?=\\d)")).map { it.toInt() }
+                        boards[boardNumber]!!.add(numbersInRow.map { boardNumbers[it] })
+
+                        if (index == inputLines.lastIndex) {
+                            val rows = boards[boardNumber]!!
+                            val columns = mutableListOf<List<BoardNumber>>()
+                            for (rowNumber in 0..rows.lastIndex) {
+                                columns.add(rows.map { it[rowNumber] })
+                            }
+                            boards[boardNumber]!!.addAll(columns)
+                        }
+                    }
+                }
+            }
+        }
+
+        //Play game until a board wins
+        numbersDrawn.forEach playGame@{ currentNumber ->
+            boardNumbers[currentNumber].marked = true
+
+            val boardsToRemove = mutableListOf<Int>()
+            boards.entries.forEach { (boardNumber, board) ->
+                board.forEach { rowOrColumn ->
+                    if (rowOrColumn.all { it.marked }) {
+                        //Only 1 board remained, and it won!
+                        if (boards.size == 1) {
+                            //calculate score
+                            val boardScore = board.flatten().distinctBy { it.value }.filterNot { it.marked }.map { it.value }.sum()
+
+                            print("Board score is $boardScore, final number called is $currentNumber, product is ${boardScore * currentNumber}")
+                            print("\nDONE :D")
+                            return
+                        } else {
+                            boardsToRemove.add(boardNumber)
+                        }
+                    }
+                }
+            }
+            boardsToRemove.forEach { boards.remove(it) }
+        }
+    }
+}
+
+data class BoardNumber(
+    val value: Int,
+    var marked: Boolean = false
+)
