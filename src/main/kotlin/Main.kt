3f8cf71cd2f4a7f1f1297e973e0d0f324c224f78
Erik <gundy.goo@gmail.com>
2021-12-03 20:34:26 -0600

    Completed day 3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,10 @@
 import solutions.aoc2021.Day1
 import solutions.aoc2021.Day2
+import solutions.aoc2021.Day3
 
 fun main() {
     try {
-        Day2().solution2()
+        Day3().solution2()
     } catch (exception: Exception) {
         print("\n$exception ${exception.stackTrace.map { "\nat -> $it" }.joinToString("")} \n")
     }
---
+++ src/main/kotlin/solutions/aoc2021/Day3.kt
@@ -0,0 +1,63 @@
+package solutions.aoc2021
+
+import AoC2021Problem
+
+class Day3: AoC2021Problem() {
+    fun solution1() {
+        val counters = Array(12) { index -> PositionalBitCounter(index)}
+        inputFile.readLines().map { it.trim() }.map { it.reversed() }.forEach {
+            it.forEachIndexed { index, bit ->
+                val positionCounter = counters[index]
+
+                if (bit == '0') positionCounter.zeros += 1
+                else positionCounter.ones += 1
+            }
+        }
+
+        val gammaRate = Integer.parseInt(counters.map { if (it.ones > it.zeros) '1' else '0' }.reversed().joinToString(""), 2)
+        val epsilonRate = Integer.parseInt(counters.map { if (it.ones > it.zeros) '0' else '1' }.reversed().joinToString(""), 2)
+
+        print("Gamma rate is $gammaRate, epsilon rate is $epsilonRate, product is ${gammaRate * epsilonRate}")
+        print("\nDONE :D")
+    }
+
+    fun solution2() {
+        val inputs = inputFile.readLines().map { it.trim() }
+
+        val oxygenRating = Integer.parseInt(evaluateDiagnostic(inputs, 0, Frequency.MOST), 2)
+        val co2Rating = Integer.parseInt(evaluateDiagnostic(inputs, 0, Frequency.LEAST), 2)
+
+        print("Oxygen Generator Rating is $oxygenRating, CO2 Scrubber Rating is $co2Rating, product is ${oxygenRating * co2Rating}")
+        print("\nDONE :D")
+    }
+
+    private fun evaluateDiagnostic(inputs: List<String>, index: Int, frequency: Frequency): String {
+        val groupedInputs = inputs.groupBy { it[index] }
+
+        return when (frequency) {
+            Frequency.MOST -> {
+                val targetSet = groupedInputs.maxByOrNull { (_, values) -> values.size }!!.value
+
+                if (targetSet.size == 1) targetSet.first()
+                else evaluateDiagnostic(targetSet, index + 1, frequency)
+            }
+            Frequency.LEAST -> {
+                val targetSet = groupedInputs.minByOrNull { (_, values) -> values.size }!!.value
+
+                if (targetSet.size == 1) targetSet.first()
+                else evaluateDiagnostic(targetSet, index + 1, frequency)
+            }
+        }
+    }
+
+    enum class Frequency {
+        MOST,
+        LEAST
+    }
+}
+
+data class PositionalBitCounter(
+    val index: Int = 0,
+    var ones: Int = 0,
+    var zeros: Int = 0
+)
