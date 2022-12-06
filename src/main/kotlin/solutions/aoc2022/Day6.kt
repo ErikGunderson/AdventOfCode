bab3c1512727d9d4794f65123af79dbf42690238
Erik <gundy.goo@gmail.com>
2022-12-06 08:54:32 -0600

    Completed day 6

--- src/main/kotlin/solutions/aoc2022/Day6.kt
+++ src/main/kotlin/solutions/aoc2022/Day6.kt
@@ -2,12 +2,28 @@ package solutions.aoc2022
 
 import solutions.utils.AoCProblem
 
-class Day6: AoCProblem() {
-    fun solution1(input: List<String>) {
+fun main() {
+    Day6().runAllSolutions()
+}
 
+class Day6 : AoCProblem() {
+    fun solution1(input: List<String>) {
+        print("Start-of-packet last char is at position ${scanStringForUniqueSubstring(input.first(), 4)}")
     }
 
     fun solution2(input: List<String>) {
+        print("Start-of-message last char is at position ${scanStringForUniqueSubstring(input.first(), 14)}")
+    }
 
+    private fun scanStringForUniqueSubstring(input: String, substringLength: Int) : Int {
+        input.windowed(substringLength, transform = { it.toList() }).run scanWindows@ {
+            forEachIndexed { index, window ->
+                if (window.distinct().size == substringLength) {
+                    return index + substringLength
+                }
+            }
+        }
+
+        return -1
     }
 }
