d735d427e4982b2771dacbcf3c1a212223316cc7
Erik <gundy.goo@gmail.com>
2024-12-01 09:58:58 -0600

    Completed day 1

--- src/main/kotlin/solutions/aoc2024/Day1.kt
+++ src/main/kotlin/solutions/aoc2024/Day1.kt
@@ -1,6 +1,7 @@
 package solutions.aoc2024
 
 import solutions.utils.AoCProblem
+import kotlin.math.absoluteValue
 
 fun main() {
     Day1().runAllSolutions()
@@ -8,10 +9,47 @@ fun main() {
 
 class Day1: AoCProblem() {
     fun solution1(input: List<String>) {
+        val left = mutableListOf<Long>()
+        val right = mutableListOf<Long>()
 
+        input.forEach { line -> line.split("   ").let {
+            left.add(it[0].toLong())
+            right.add(it[1].toLong())
+        }}
+
+        left.sort()
+        right.sort()
+
+        var locationIdDiffSum: Long = 0
+
+        left.forEachIndexed { index, locationId ->
+            locationIdDiffSum += (locationId - right[index]).absoluteValue
+        }
+
+        println("Location ID differences summed: $locationIdDiffSum")
     }
 
     fun solution2(input: List<String>) {
+        val left = mutableListOf<Long>()
+        val right = mutableMapOf<Long, Int>()
 
+        input.forEach { line -> line.split("   ").let {
+            left.add(it[0].toLong())
+
+            it[1].toLong().let { rightVal ->
+                if (right.containsKey(rightVal)) {
+                    right[rightVal] = right[rightVal]!! + 1
+                } else {
+                    right[rightVal] = 1
+                }
+            }
+        }}
+
+        var similarityScoreSum: Long = 0
+        left.forEach { leftVal ->
+            similarityScoreSum += leftVal * (right[leftVal] ?: 0)
+        }
+
+        println("Similarity scores summed: $similarityScoreSum")
     }
 }
