6de65fab0ba45e7b6d244e0850056aacb155f1e9
Erik <gundy.goo@gmail.com>
2024-12-03 10:57:45 -0600

    Completed day 3

--- src/main/kotlin/solutions/aoc2024/Day2.kt
+++ src/main/kotlin/solutions/aoc2024/Day2.kt
@@ -8,10 +8,52 @@ fun main() {
 
 class Day2: AoCProblem() {
     fun solution1(input: List<String>) {
+        var safeReportCount = 0
 
+        input.forEach { line -> line.split(" ").map { it.toInt() }.let { reactorReport ->
+            val reactorLevelTrend = if (reactorReport[0] - reactorReport[1] > 0) {
+                ReactorLevelTrend.DECREASING
+            } else ReactorLevelTrend.INCREASING
+
+            var safeReport = true
+            reactorReport.windowed(2).map { (left, right) -> left - right }.forEach { pairDiff ->
+                if (pairDiff == 0
+                    || (reactorLevelTrend == ReactorLevelTrend.DECREASING && pairDiff !in 1..3)
+                    || (reactorLevelTrend == ReactorLevelTrend.INCREASING && pairDiff !in -3..-1)) {
+                     safeReport = false
+                }
+            }
+
+            if (safeReport) safeReportCount++
+        }}
+
+        println("Safe Report Count: $safeReportCount")
     }
 
     fun solution2(input: List<String>) {
+        var safeReportCount = 0
 
+        input.forEach { line -> line.split(" ").map { it.toInt() }.let { reactorReport ->
+            val reactorLevelTrend = if (reactorReport[0] - reactorReport[1] > 0) {
+                ReactorLevelTrend.DECREASING
+            } else ReactorLevelTrend.INCREASING
+
+            var badPairCount = 0
+            reactorReport.windowed(2).map { (left, right) -> left - right }.forEach { pairDiff ->
+                if (pairDiff == 0
+                    || (reactorLevelTrend == ReactorLevelTrend.DECREASING && pairDiff !in 1..3)
+                    || (reactorLevelTrend == ReactorLevelTrend.INCREASING && pairDiff !in -3..-1)) {
+                    badPairCount++
+                }
+            }
+
+            if (badPairCount in 0..1) safeReportCount++
+        }}
+
+        println("Safe Report Count: $safeReportCount")
     }
+}
+
+enum class ReactorLevelTrend {
+    INCREASING, DECREASING
 }
--- src/main/kotlin/solutions/aoc2024/Day3.kt
+++ src/main/kotlin/solutions/aoc2024/Day3.kt
@@ -7,11 +7,47 @@ fun main() {
 }
 
 class Day3: AoCProblem() {
-    fun solution1(input: List<String>) {
+    override val testInput: List<String>
+        get() = listOf("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")
 
+    fun solution1(input: List<String>) {
+        val mulRegex = Regex("mul\\(\\d{1,3},\\d{1,3}\\)")
+
+        val sum: Long = mulRegex.findAll(input.joinToString("")).sumOf { matchResult ->
+            matchResult.value.removePrefix("mul(").removeSuffix(")").split(",").let { digits ->
+                digits[0].toLong() * digits[1].toLong()
+            }
+        }
+
+        println("Sum of mul(X,Y) commands: $sum")
     }
 
     fun solution2(input: List<String>) {
+        val mulRegex = Regex("(do\\(\\))|(don't\\(\\))|(mul\\(\\d{1,3},\\d{1,3}\\))")
 
+        var skipNextMul = false
+        val sum: Long = mulRegex.findAll(input.joinToString("")).sumOf { matchResult ->
+            when (matchResult.value) {
+                "do()" -> {
+                    skipNextMul = false
+                    0L
+                }
+                "don't()" -> {
+                    skipNextMul = true
+                    0L
+                }
+                else -> {
+                    if (skipNextMul) {
+                        0L
+                    } else {
+                        matchResult.value.removePrefix("mul(").removeSuffix(")").split(",").let { digits ->
+                            digits[0].toLong() * digits[1].toLong()
+                        }
+                    }
+                }
+            }
+        }
+
+        println("Sum of mul(X,Y) commands: $sum")
     }
 }
